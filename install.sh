bab6688b2f4a8b08e8d5b42e65834c565618a47d
cpy-dev <cpy@debian-BULLSEYE-live-builder-AMD64>
2023-06-05 15:03:16 +0200

    added network and disk speed, edited install script and readme file

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
-mkdir /usr/share/sysfetch
-mkdir $HOME/.sysfetch
+if [ "$(whoami)" != "root" ]; then
+  echo "This script must be executed as super-user"
+  exit 0
+fi
 
-cp conf.json $HOME/.sysfetch
-cp sysfetch /usr/local/bin
-cp distros.py /usr/share/sysfetch
+mkdir /usr/share/sysfetch &>> /dev/null
+mkdir $HOME/.sysfetch &>> /dev/null
+
+cp conf.json $HOME/.sysfetch &>> /dev/null
+cp sysfetch /usr/local/bin &>> /dev/null
+cp distros.py /usr/share/sysfetch &>> /dev/null
+
+echo "Succesful execution. Remeber to install the Python package \"psutil\" if you haven't already"
