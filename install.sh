57ec88ae7175a158b57b6daa5aee26c7d6e9a2c1
root-debian-main <a@a>
2023-06-11 13:35:00 +0200

    updated install script

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-sudo mkdir /usr/share/sysfetch 2>&1 /dev/null
-sudo cp sysfetch /usr/local/bin 2>&1 /dev/null
-sudo cp distros.py /usr/share/sysfetch 2>&1 /dev/null
+sudo mkdir /usr/share/sysfetch &>> /dev/null
+sudo cp sysfetch /usr/local/bin &>> /dev/null
+sudo cp distros.py /usr/share/sysfetch &>> /dev/null
 
-mkdir $HOME/.sysfetch 2>&1 /dev/null
-cp conf.json $HOME/.sysfetch 2>&1 /dev/null
+mkdir $HOME/.sysfetch &>> /dev/null
+cp conf.json $HOME/.sysfetch &>> /dev/null
 
 echo "Succesful execution. Remeber to install the Python package \"psutil\" if you haven't already"
