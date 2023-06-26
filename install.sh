3fd3e4cf88453a9655f6644f65d08423c7518b3e
root-debian-main <a@a>
2023-06-26 08:27:47 +0200

    bug fix

--- install.sh
+++ install.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
-sudo mkdir /usr/share/sysfetch &>> /dev/null
-sudo cp sysfetch /usr/local/bin &>> /dev/null
-sudo cp distros.py /usr/share/sysfetch &>> /dev/null	
 
-mkdir $HOME/.sysfetch &>> /dev/null
-cp conf.json $HOME/.sysfetch &>> /dev/null
+if [ -z "$1" ]; then
+	echo -e "Sysfetch installation script\n"
+	echo -e "./install.sh OPTION\n"
+	echo -e "Options:"
+	echo -e "	full			Full installation: creates /$HOME/.sysfetch directory and copies in it standard version of \"conf.json\"," 
+	echo -e "    				it also copies the \"sysfetch\" script in /usr/local/bin\n"
+	echo -e "	update 			Only copies \"sysfetch\" script in /usr/local/bin"
+	exit 0
 
-echo "Succesful execution. Remeber to install the Python package \"psutil\" if you haven't already"
+elif [ "$1" == "full" ]; then
+	sudo mkdir /usr/share/sysfetch &>> /dev/null
+	sudo cp sysfetch /usr/local/bin &>> /dev/null
+	sudo cp distros.py /usr/share/sysfetch &>> /dev/null	
+
+	mkdir $HOME/.sysfetch &>> /dev/null
+	cp conf.json $HOME/.sysfetch &>> /dev/null
+
+	echo "Succesful full installation. Remeber to install the Python package \"psutil\" if you haven't already"
+
+elif [ "$1" == "update" ]; then
+	sudo cp sysfetch /usr/local/bin &>> /dev/null
+	echo "Succesful update"
+fi
